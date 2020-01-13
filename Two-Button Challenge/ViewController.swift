d272590f29f8e81aca7f9c95da6d5799eea2f0de
yeindaw <yedawin@gmail.com>
2020-01-13 15:54:14 -0500

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Dawin Ye on 1/13/20.
+//  Copyright © 2020 Dawin Ye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
